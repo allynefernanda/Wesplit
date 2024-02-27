f0e7bd645e3e8e667c3f58810176e2fe29487ec5
allynefernanda <fernandavsob@gmail.com>
2024-02-27 13:13:17 -0300

    criacao de das navegacoes a mais

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,71 +1,115 @@
-
-//  ContentView.swift
-//  Wesplit
-//
-//  Created by allyne fernanda  on 20/02/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                NavigationLink(destination: SecondView()) {
+                    Text("Ir para a Tela Principal")
+                }
+                Spacer()
+            }.navigationBarTitleDisplayMode(.inline)
+            .padding()
+            .navigationTitle("Pagina Inicial")
+        }
+    }
+}
+
+struct SecondView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
     
-    let tipPercentages = [10,15,25,50,75] //armazena as porcentagens de gorjeta disponíveis
+    let tipPercentages = [10, 15, 25, 50, 75]
     
-    //uma função computada, que calcula o valor total por pessoa e 
-    var totalPerPerson: Double{
-        let peopleCount = Double(numberOfPeople+2)
-        let TipSelection = Double(tipPercentage)
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
         
-        let tipValue = checkAmount / 100 * TipSelection
+        let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
         
         return amountPerPerson
-        
     }
     
-   // Este é o corpo principal da estrutura de visualização, onde a aparência e o comportamento da interface do usuário são definidos.
     var body: some View {
-        NavigationStack{
-            Form{
-                Section{
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad).focused($amountIsFocused)
-                    
-                    Picker("Number of people", selection: $numberOfPeople){
-                        ForEach(2..<100){
-                            Text("\($0) people")
-                        }
-                    }//.pickerStyle(.navigationLink)
-                }
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
                 
-                Section("How much do you want to tip?"){
-                    //Text("How much do you want to tip?")
-                    Picker("tip percentage", selection: $tipPercentage){
-                        ForEach(tipPercentages, id: \.self){
-                            Text($0, format: .percent)
-                        }//.focused($amountIsFocused)
-                    }.pickerStyle(.segmented)
+                Picker("Number of People", selection: $numberOfPeople) {
+                    ForEach(2..<100) {
+                        Text("\($0) people")
+                    }
                 }
+            }
+            
+            Section(header: Text("How much do you want to tip?")) {
+                Picker("Tip Percentage", selection: $tipPercentage) {
+                    ForEach(tipPercentages, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            
+                
                 
                 Section{
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad).focused($amountIsFocused)
                 }
-            }.navigationTitle("WeSplit")
-                .toolbar{
-                    if amountIsFocused{
-                        Button("Done"){
-                            amountIsFocused = false
-                        }
+            
+            Section {
+                NavigationLink(destination: ThreeView()) {
+                    Text("Ir para a segunda tela")
+                }
+                
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
                     }
-                }.navigationBarTitleDisplayMode(.inline)
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
     
 }
-    #Preview {
-        ContentView()
+struct SecondNavigationView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Estamos na segunda navegação!")
+                NavigationLink(destination: SecondView()) {
+                    Text("Ir para a Segunda Tela")
+                }
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Segunda Navegação")
+        }
     }
+}
+
+struct ThreeView: View {
+    var body: some View {
+        Text("Segunda Tela")
+            .navigationTitle("Segunda Tela")
+    }
+}
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+
