380abd8991a1a7486ed819e601965dc1781a1dd8
allynefernanda <fernandavsob@gmail.com>
2024-02-27 12:26:43 -0300

    comecinho

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -13,87 +13,59 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
     
-    let tipPercentages = [10, 15, 25, 50, 75]
+    let tipPercentages = [10,15,25,50,75] //armazena as porcentagens de gorjeta disponíveis
     
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
-        let tipSelection = Double(tipPercentage)
+    //uma função computada, que calcula o valor total por pessoa e 
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople+2)
+        let TipSelection = Double(tipPercentage)
         
-        let tipValue = checkAmount / 100 * tipSelection
+        let tipValue = checkAmount / 100 * TipSelection
         let grandTotal = checkAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
         
         return amountPerPerson
+        
     }
     
+   // Este é o corpo principal da estrutura de visualização, onde a aparência e o comportamento da interface do usuário são definidos.
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                        .keyboardType(.decimalPad)
-                        .focused($amountIsFocused)
+        NavigationStack{
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad).focused($amountIsFocused)
                     
-                    Picker("Number of People", selection: $numberOfPeople) {
-                        ForEach(2..<100) {
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
                             Text("\($0) people")
                         }
-                    }
+                    }//.pickerStyle(.navigationLink)
                 }
                 
-                Section(header: Text("How much do you want to tip?")) {
-                    Picker("Tip Percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
-                            Text("\($0)%")
+                Section("How much do you want to tip?"){
+                    //Text("How much do you want to tip?")
+                    Picker("tip percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }//.focused($amountIsFocused)
+                    }.pickerStyle(.segmented)
+                }
+                
+                Section{
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad).focused($amountIsFocused)
+                }
+            }.navigationTitle("WeSplit")
+                .toolbar{
+                    if amountIsFocused{
+                        Button("Done"){
+                            amountIsFocused = false
                         }
                     }
-                    .pickerStyle(.segmented)
-                }
-                
-                Section {
-                    NavigationLink(destination: SecondNavigationView()) {
-                        Text("Ir para a Segunda Navegação")
-                    }
-                }
-            }
-            .navigationTitle("WeSplit")
-            .toolbar {
-                if amountIsFocused {
-                    Button("Done") {
-                        amountIsFocused = false
-                    }
-                }
-            }
-            .navigationBarTitleDisplayMode(.inline)
+                }.navigationBarTitleDisplayMode(.inline)
         }
     }
+    
 }
-
-struct SecondView: View {
-    var body: some View {
-        Text("Segunda Tela")
-            .navigationTitle("Segunda Tela")
-    }
-}
-
-struct SecondNavigationView: View {
-    var body: some View {
-        NavigationView {
-            VStack {
-                Text("Estamos na segunda navegação!")
-                NavigationLink(destination: SecondView()) {
-                    Text("Ir para a Segunda Tela")
-                }
-                Spacer()
-            }
-            .padding()
-            .navigationTitle("Segunda Navegação")
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+    #Preview {
         ContentView()
     }
-}
